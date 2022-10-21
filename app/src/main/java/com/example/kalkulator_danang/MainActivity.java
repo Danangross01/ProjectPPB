d21853d8b512d8e578041712fac98417cd89e554
Danangross01 <danangprasetya123123@gmail.com>
2022-10-21 15:33:00 +0700

    Project UTS Kalkulator

---
+++ app/src/main/java/com/example/kalkulator_danang/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.kalkulator_danang;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void persegi(View view) {
+        Intent intent = new Intent(MainActivity.this, persegi.class);
+        startActivity(intent);
+    }
+
+    public void persegi_panjang(View view) {
+        Intent intent = new Intent(MainActivity.this, persegi_panjang.class);
+        startActivity(intent);
+    }
+
+    public void segitiga(View view) {
+        Intent intent = new Intent(MainActivity.this, segitiga.class);
+        startActivity(intent);
+    }
+
+    public void lingkaran(View view) {
+        Intent intent = new Intent(MainActivity.this, lingkaran.class);
+        startActivity(intent);
+    }
+
+
+
+
+}
